8278459d18be5fe2ae5fdae042c1f6c8cff1883f
trapcodeio <trapcode.io@gmail.com>
2020-02-17 05:03:46 +0400

    Initial Commit

---
+++ LearnUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnUi
+//
+//  Created by Evil on 17/02/2020.
+//  Copyright © 2020 TrapCode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
